eb7350b4a15391b025a6e8fdf57cff73f4a67367
Serbanescu Alina <stanalinaionela@yahoo.ro>
2022-05-17 21:59:39 +0300

    curs 17.05

---
+++ M01S02/ex01/js/utils.js
@@ -0,0 +1,3 @@
+console.log((45 * 82).toString());
+console.log((13 ** 2 + 2 * 13 * 41 + 41 ** 2).toString());
+console.log((Math.pow(15 / 2, 2) * Math.PI).toString());
---
+++ M01S02/ex01/shapes.js
@@ -0,0 +1,1 @@
+console.log(Math.pow(16, 2).toString());
