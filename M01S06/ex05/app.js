426319e7332455a209bd405fdae3e555c99f8db5
Serbanescu Alina <stanalinaionela@yahoo.ro>
2022-06-30 16:49:41 +0300

    curs 21 iunie

--- M01S06/ex05/app.js
+++ M01S06/ex05/app.js
@@ -1,5 +1,48 @@
-(function() {
-  document.addEventListener('DOMContentLoaded', function() {
-    
+// IIFE - immedietaly invoked function expression
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
+    const showButton = document.querySelector('#showButton');
+    const toggleEventButton = document.querySelector('#toggleEventButton');
+    const messageClass = 'message';
+    let eventBound = true;
+
+    showButton.addEventListener('click', showAlert);
+
+    // hoisting
+    function showAlert() {
+      alert('Butonul a fost apasat');
+    }
+
+    function showMessage(message) {
+      let paragraphElement = document.querySelector(`.${messageClass}`);
+
+      if (paragraphElement === null) {
+        paragraphElement = document.createElement('p');
+        paragraphElement.classList.add(messageClass);
+        paragraphElement.innerText = message;
+
+        document.body.append(paragraphElement);
+      } else {
+        paragraphElement.innerText = message;
+      }
+    }
+
+    toggleEventButton.addEventListener('click', function () {
+      if (eventBound === true) {
+        showButton.removeEventListener('click', showAlert);
+
+        eventBound = false;
+
+        this.innerText = 'Porneste afisarea';
+        showMessage('Alerta NU va fi afisata');
+      } else {
+        showButton.addEventListener('click', showAlert);
+
+        eventBound = true;
+
+        this.innerText = 'Opreste afisarea';
+        showMessage('Alerta va fi afisata');
+      }
+    });
   });
 })();
